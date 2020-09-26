ee2aeca0b0de19a4f223d8622307dd72ba9b5072
Emiliano <emiliano.2.farias@hotmail.com>
2020-09-26 09:57:47 -0300

    V1

---
+++ interactividad.js
@@ -0,0 +1,28 @@
+const media = window.matchMedia("screen and (max-width: 767px)");
+
+const menu = document.querySelector('.menu')
+// console.log("menu");
+const burgerButtom = document.querySelector("#burger-menu");
+// console.log("burgerButtom");
+
+media.addListener(validation)
+
+function validation(event) {
+  if (event.matches) {
+     console.log(event.matches);
+     burgerButtom.addEventListener("click", show)
+  }
+  else {
+     console.log("no activado");
+     burgerButtom.removeEventListener("click", show)
+  }
+}
+
+function show() {
+  if (menu.classList.contains("is-active")) {
+    menu.classList.remove('is-active');
+  }
+  else {
+    menu.classList.add('is-active');
+  }
+}
